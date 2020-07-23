0acd9cba5be7b91cff37b70b9ccd1658fd994ac8
anjastvtz <as314@hdm-stuttgart.de>
2020-07-23 09:25:58 +0200

    added dist folder

--- deploy.sh
+++ deploy.sh
@@ -9,17 +9,12 @@ vuepress build public
 # navigate into the build output directory
 cd public/.vuepress/dist
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
 git push -f git@github.com:horizonfleet/horizonfleet.github.io.git master
 
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
 cd -
